a0395d34cfd9defe28990b57950dc06a4fc98c5c
TeamStarloft <monster567456@gmail.com>
2026-02-10 22:25:18 +0000

    Update blox.php

--- config/blox.php
+++ config/blox.php
@@ -1,35 +1,10 @@
-<?php
-/**
- * MIT License
- *
- * Copyright (c) 2021-2024 FoxxoSnoot, Hurricane
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 use App\Models\Item;
 
 return [
     'flood_time' => 15,
     'featured_items' => [1, 2],
     'render' => [
-        'url' => 'https://rend.bloxcity.net',
+        'url' => 'https://server.bloxcity.net',
         'key' => 'key'
     ],
 	'item_notifier' => [
@@ -60,18 +35,18 @@ return [
     ],
     'domains' => [
         'storage' => env('STORAGE_DOMAIN'),
-        'blog' => 'https://blog.bloxcreate.com',
-        'corp' => 'https://corp.bloxcreate.com'
+        'blog' => 'https://blog.bloxcity.net',
+        'corp' => 'https://corp.bloxcity.net'
     ],
     'emails' => [
-        'support' => 'hello@bloxcreate.com',
+        'support' => 'hello@bloxcity.net',
         'moderation' => 'moderation@bloxcreate.com',
-        'careers' => 'hello@bloxcreate.com',
-        'payments' => 'payments@bloxcreate.com'
+        'careers' => 'hello@bloxcity.net',
+        'payments' => 'payments@bloxcity.net'
     ],
     'socials' => [
         'discord' => 'https://discord.gg/bloxcity',
-        'twitter' => 'https://twitter.com/bloxcreategame',
+        'twitter' => 'https://twitter.com/bloxcitynet',
         'youtube' => '/'
     ],
     'forum_banners' => [
